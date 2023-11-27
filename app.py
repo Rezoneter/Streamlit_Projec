3a11a48284f65dd0dd0d9c02f78c4b40037407e0
Rezoneter <150760616+Rezoneter@users.noreply.github.com>
2023-11-27 14:45:45 +0900

    add app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
